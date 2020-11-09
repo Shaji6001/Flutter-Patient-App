b6e16570c9c277dbbba423614d1eb19bcb23db7b
Shaji6001 <shajishaijigmail.com>
2020-11-09 17:33:16 +0300

    Txt controller added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 
 void main()=>runApp(PatientApp());
 class PatientApp extends StatelessWidget {
+  TextEditingController getPatId=TextEditingController();
+  TextEditingController getPatName=TextEditingController();
+  TextEditingController getSymp=TextEditingController();
+  TextEditingController getAge=TextEditingController();
+  TextEditingController getMedi=TextEditingController();
+  TextEditingController getDrName=TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,38 +23,51 @@ class PatientApp extends StatelessWidget {
           child: SingleChildScrollView(
             child: Column(
               children: [
-               TextField(
+               TextField(controller: getPatId,
                  decoration: InputDecoration(
                    hintText: "Patient ID",border: OutlineInputBorder()
                  ),
                ) ,
                 SizedBox(height: 10.0,),
-                TextField(
+                TextField(controller: getPatName,
                     decoration: InputDecoration(
                       hintText: "Patient Name",border: OutlineInputBorder()
                     )
                 ),SizedBox(height: 10.0,),
-                TextField(
+                TextField(controller: getSymp,
                   decoration: InputDecoration(
                     hintText: "Symptoms",border: OutlineInputBorder()
                   )
                 ),SizedBox(height: 10.0,),
-                TextField(
+                TextField(controller: getAge,
                   decoration: InputDecoration(
                       hintText: "Age",border: OutlineInputBorder()
                   )
                 ),SizedBox(height: 10.0,),
-                TextField(
+                TextField(controller: getMedi,
                   decoration: InputDecoration(
                       hintText: "Medicine",border: OutlineInputBorder()
                   )
                 ),SizedBox(height: 10.0,),
-                TextField(
+                TextField(controller: getDrName,
                   decoration: InputDecoration(
                       hintText: "Doctor Name",border: OutlineInputBorder()
                   )
                 ),SizedBox(height: 10.0,),
                 RaisedButton(onPressed: (){
+                  String getpatid=getPatId.text;
+                  String getpatname=getPatName.text;
+                  String getsym=getSymp.text;
+                  String getage=getAge.text;
+                  String getmed=getMedi.text;
+                  String getdrname=getDrName.text;
+                  print(getpatid);
+                  print(getpatname);
+                  print(getsym);
+                  print(getage);
+                  print(getmed);
+                  print(getdrname);
+
                 },child: Text("SUBMIT"),color: Colors.blue,)
               ],
             ),
